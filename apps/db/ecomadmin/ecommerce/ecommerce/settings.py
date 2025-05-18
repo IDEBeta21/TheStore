34a85119948dbb3bb76f4c4dbc2ae0fad7043856
IDEBeta21 <deladia.ian.bscs2018@gmail.com>
2025-05-19 01:06:01 +0800

    initial django migration

--- apps/db/ecomadmin/ecommerce/ecommerce/settings.py
+++ apps/db/ecomadmin/ecommerce/ecommerce/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecom-db',
+        'USER': 'admin',
+        'PASSWORD': 'adminpassword',
+        'HOST': 'localhost',
+        'PORT': '5436',
     }
 }
 
--- apps/db/ecomadmin/ecommerce/thestore/models.py
+++ apps/db/ecomadmin/ecommerce/thestore/models.py
@@ -1,3 +1,51 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+class StoreTypes(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = "store_types"
+
+class Stores(models.Model):
+    external_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, auto_created=True)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    store_types = models.ForeignKey(StoreTypes, on_delete=models.CASCADE, related_name="stores_store_types")
+    is_deleted = models.BooleanField
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.CharField(max_length=50)
+    updated_at = models.DateTimeField(null=True)
+    updated_by = models.DateField(null=True)
+
+    class Meta:
+        db_table = "stores"
+
+class ProductTypes(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = "product_types"
+
+class Products(models.Model):
+    external_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, auto_created=True)
+    store_id = models.ForeignKey(Stores, on_delete=models.CASCADE, related_name="products_stores")
+    image = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    product_type = models.ForeignKey(ProductTypes, on_delete=models.CASCADE, related_name="products_product_types")
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_available = models.BooleanField
+    is_deleted = models.BooleanField
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.CharField(max_length=50)
+    updated_at = models.DateTimeField(null=True)
+    updated_by = models.DateField(null=True)
+
+    class Meta:
+        db_table = "products"
+    
+
+
+    
