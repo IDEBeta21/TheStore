44de0da76e65c39d4f3fe2f2da06ca8d9d3b23e3
Ian Deladia (Ermino) <kitsmype416@gmail.com>
2025-07-29 23:51:08 +0800

    initial addition of dtos and services

---
+++ apps/api/EcomService.API/Models/Dto/Product/GetProductDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcomService.API.Models.Dto.Product
+{
+    public class GetProductDto
+    {
+        
+    }
+}
---
+++ apps/api/EcomService.API/Models/Dto/Store/GetStoreDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcomService.API.Models.Dto.Store
+{
+    public class GetStoreDto
+    {
+        
+    }
+}
